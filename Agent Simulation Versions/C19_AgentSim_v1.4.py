a28b704cd6fe5017bf926fd012b391708f80fa0a
Foxi-GB <itsShibs@gmail.com>
2022-12-16 16:45:06 +0000

    Drawing Surface Agents and their Cones
    Drawing agents as a surface along with the cones used to represent their mouth.
    
    Current issues include the agent being drawn next to their cone without it being correctly attached.

--- Agent Simulation Versions/C19_AgentSim_v1.4.py
+++ Agent Simulation Versions/C19_AgentSim_v1.4.py
@@ -8,24 +8,47 @@ FPS = 60
 agentSize = 10
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-class Agent:
+class Image: 
     def __init__(self):
+        self.player = self.create_player()
+
+    def create_player(self):
+        surface = pygame.Surface((20, 20), pygame.SRCALPHA, 32)
+        surface = surface.convert_alpha()
+        pygame.draw.circle(surface, "black", (10,10), 10)
+        pygame.draw.line(surface, "green", (10, 10), (20, 10))
+        return surface
+
+class Agent:
+    def __init__(self, image, position):
+
         # Agent Coordinates
         self.x = random.randrange(10, WIDTH -10)
         self.y = random.randrange(10, HEIGHT -10)
         self.pos = [self.x, self.y]
 
+        self.oimage = image
+        self.image = image
+        self.rect = image.get_rect(center = position)
+        self.center = pygame.Vector2(self.rect.center)
+
         # Agent Rotation
         self.rotation = 0 
+        self.angle = 0 
+        self.speed = 50
+        self.turnSpeed = 0
+        self.vector = pygame.Vector2()
+        self.vector.from_polar((1, 0))
 
         # Agent Infected
         self.infected = bool(np.random.choice([0,1],1,p=[0.90,0.10]))
 
     def draw(self):
-        if(self.infected):
-            pygame.draw.circle(screen, (255, 0, 0), (self.x,self.y), agentSize) 
-        else:
-            pygame.draw.circle(screen, (32, 32, 32), (self.x,self.y), agentSize)
+        
+        # if(self.infected):
+        #     pygame.draw.circle(screen, (255, 0, 0), (self.x,self.y), agentSize) 
+        # else:
+        #     pygame.draw.circle(screen, (32, 32, 32), (self.x,self.y), agentSize)
         
         Agent.drawCone(self.x, self.y, self.rotation)
     
@@ -45,18 +68,50 @@ class Agent:
 
         pygame.draw.polygon(screen, (255,0,0), ((ptX_x, ptX_y), (ptY_x, ptY_y), (ptZ_x, ptZ_y))) # Draw Cone
 
+    def move(self):
+        self.rotation = 0
+        forward = 0
+        delta = 0
+
+        if(np.random.choice([0,1],1,p=[0.50,0.50]) == 0):
+            self.rotation -= 5
+        else:
+            self.rotation += 5
+
+        if(np.random.choice([0,1],1,p=[0.90,0.10]) == 0):
+            forward += 1
+        else:
+            forward -= 1
+
+        if self.rotation != 0:
+            self.angle += delta * self.turnSpeed * self.rotation
+            self.image = pygame.transform.rotate(self.oimage, -self.angle)
+            self.rect = self.image.get_rect(center=self.rect.center)
+            self.vector.from_polar((1, self.angle))
+        
+        if forward != 0:
+            self.center += forward * self.vector * delta * self.speed
+            self.rect.center = self.center
+
 class Simulation:
 
-    def __init__(self, title, fps):
+    def __init__(self, title, fps, size, flags):
         pygame.display.set_caption(title)
+        self.surface = pygame.display.set_mode(size, flags)
+
         self.width = WIDTH
         self.height = HEIGHT
         self.screen = screen
         self.fps = fps
 
+        self.rect = self.surface.get_rect()
+
+        self.images = Image()
+        
+
         self.agents = []
         for i in range(3):
-            Agents = Agent()
+            Agents = Agent(self.images.player, self.rect.center)
             self.agents.append(Agents)
 
     def simLoop(self):
@@ -66,12 +121,14 @@ class Simulation:
                     pygame.quit()
                     sys.exit()
 
-            screen.fill((255,255,255))
+            self.surface.fill('white')
 
             for idx, i in enumerate(self.agents):
+                self.surface.blit(self.agents[idx].image, self.agents[idx].pos)
                 i.draw()
+                i.move()
             pygame.display.update() 
             pygame.time.Clock().tick(FPS)
 
-sim = Simulation("Covid 19 Agent Simulation", 60)
+sim = Simulation("Covid 19 Agent Simulation", 60, (800,600), 0)
 sim.simLoop()
